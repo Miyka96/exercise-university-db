883de228c41772f41cf77d6111c70fb11d31369e
Miyka96 <micaelaseminara@outlook.it>
2022-05-17 23:21:09 +0200

    teacher seeder done

--- database/seeds/CourseSeeder.php
+++ database/seeds/CourseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Models\Course;
+use App\Models\Degree;
 
 class CourseSeeder extends Seeder
 {
@@ -9,8 +12,14 @@ class CourseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        $degree= Degree::all();
+        $degreeId= $degree->pluck('id')->all();
+        for ($i=0; $i < 500; $i++) { 
+
+            $course = new Course;          
+            $course->degree_id = $faker->optional()->randomElement($degreeId);
+        }
     }
 }
--- database/seeds/TeacherSeeder.php
+++ database/seeds/TeacherSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Models\Teacher;
 
 class TeacherSeeder extends Seeder
 {
@@ -9,8 +11,21 @@ class TeacherSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i < 500 ; $i++) { 
+            
+            $teacher = new Teacher;
+
+            $teacher->name= $faker->firstName();
+            $teacher->surname= $faker->lastName();
+            $teacher->phone= $faker->phoneNumber();
+            $teacher->email= $faker->email();
+            $teacher->office_address= $faker->streetAddress();
+            $teacher->office_number = $faker->numberBetween(0, 700);
+
+            $teacher->save();
+
+        }
     }
 }
